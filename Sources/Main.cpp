bbadca15bde5e253c1dd2136dc0d6497d7e7843a
DESRUET Antoine <antoine.desruet@epitech.eu>
2022-07-09 21:34:29 +0200

    feat: simple window

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -5,7 +5,23 @@
 ** Created by antoine,
 */
 
+#include <SFML/Graphics.hpp>
+
+#include <iostream>
+
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    sf::CircleShape circle(15);
+    circle.setFillColor(sf::Color::Red);
+    sf::Event event;
+    while (window.isOpen()) {
+        window.pollEvent(event);
+        if (event.type == sf::Event::Closed)
+            window.close();
+        window.clear();
+        window.draw(circle);
+        window.display();
+    }
     return 0;
 }
